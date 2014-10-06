b0a16aa07b997974562ebc581dd8e82b273fac9f
Ian Sinnott <ian@iansinnott.com>
2014-10-07 02:08:49 +0800

    Build the basic app. Still has very limited styling. Also add gulp.

---
+++ Gulpfile.js
@@ -0,0 +1,59 @@
+// Gulp
+var gulp    = require('gulp'),
+    plumber = require('gulp-plumber'),
+    jade    = require('gulp-jade'),
+    stylus  = require('gulp-stylus'),
+    prefix  = require('gulp-autoprefixer'),
+    lr      = require('gulp-livereload'),
+    concat  = require('gulp-concat');
+
+var app   = require('./app');
+var debug = require('debug')('reference');
+app.set('port', process.env.PORT || 3000);
+
+// Other Deps
+var nib = require('nib');
+
+// Compile Stylus Files
+function compileStyles() {
+  return gulp.src('styles/style.styl')
+    .pipe(plumber())
+    .pipe(stylus({ errors: true, use: [nib()] }))
+    .pipe(prefix())
+    .pipe(gulp.dest('./public/stylesheets'));
+}
+
+// Styles can be compiled via gulp
+gulp.task('styl', compileStyles);
+
+gulp.task('serve', function() {
+  var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+  });
+});
+
+gulp.task('deps', function() {
+  gulp.src([
+    'node_modules/highlight.js/styles/github.css'
+  ]).pipe(concat('dependencies.css'))
+    .pipe(gulp.dest('public/stylesheets'));
+
+});
+
+gulp.task('watch', function() {
+
+  // Start live reload
+  var server = lr();
+
+  lr.listen();
+
+  // Inject style changes for quick iteration
+  gulp.watch('stylesheets/**/*.styl').on('change', compileStyles);
+
+  // livereload needs to see that it was in fact a CSS file that
+  // changed in order to do injection, so I need to watch the css file not the
+  // stylus file.
+  gulp.watch('public/stylesheets/style.css').on('change', server.changed);
+});
+
+gulp.task('default', ['watch']);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+var express          = require('express');
+var fs               = require('fs');
+var _                = require('lodash');
+var marked           = require('marked');
+var parseFrontMatter = require('yaml-front-matter').loadFront;
+
+var router  = express.Router();
+
+// Synchronous code highlighting with highlight.js
+marked.setOptions({
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var languages = _.remove(fs.readdirSync('./docs'), function(item) {
+    return item.match(/\.html\.markdown/);
+  });
+
+  languages = languages.map(function(item) {
+    return item.slice(0, item.indexOf('.'));
+  });
+
+  res.render('index', {
+    languages: languages
+  });
+});
+
+router.get('/:lang', function(req, res) {
+  var lang = req.params.lang;
+  var markdown,
+      markdownFile;
+
+  try {
+    markdown = fs.readFileSync('./docs/' + lang + '.html.markdown');
+  } catch (err) {
+    return res.render('error', {
+      message: "No reference found for " + lang,
+      error: {}
+    });
+  }
+
+  markdownFile = parseFrontMatter(markdown);
+
+  res.render('doc', {
+    title: lang,
+    bodyHTML: marked(markdownFile.__content)
+  });
 });
 
 module.exports = router;
