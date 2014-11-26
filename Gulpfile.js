ef33186b9bf89493066dd45a874e66a0f6e6e6e8
Ian Sinnott <ian@iansinnott.com>
2014-11-26 15:26:36 +0800

    Add font awesome do dependencies. Minimize css in build process.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,9 @@
 var gulp    = require('gulp'),
     plumber = require('gulp-plumber'),
     jade    = require('gulp-jade'),
-    stylus  = require('gulp-stylus'),
     prefix  = require('gulp-autoprefixer'),
     lr      = require('gulp-livereload'),
+    minCSS  = require('gulp-minify-css'),
     concat  = require('gulp-concat');
 
 var app   = require('./app');
@@ -12,19 +12,21 @@ var debug = require('debug')('reference');
 app.set('port', process.env.PORT || 3000);
 
 // Other Deps
-var nib = require('nib');
+var stylus  = require('gulp-stylus'),
+    jeet    = require('jeet'),
+    rupture = require('rupture'),
+    nib     = require('nib');
 
-// Compile Stylus Files
-function compileStyles() {
-  return gulp.src('styles/style.styl')
-    .pipe(plumber())
-    .pipe(stylus({ errors: true, use: [nib()] }))
-    .pipe(prefix())
-    .pipe(gulp.dest('./public/stylesheets'));
-}
 
 // Styles can be compiled via gulp
-gulp.task('styl', compileStyles);
+gulp.task('styl', function() {
+  return gulp.src('stylesheets/style.styl')
+    .pipe(plumber())
+    .pipe(stylus({ errors: true, use: [nib(), jeet(), rupture()] }))
+    .pipe(prefix())
+    .pipe(minCSS())
+    .pipe(gulp.dest('public/stylesheets'));
+});
 
 gulp.task('serve', function() {
   var server = app.listen(app.get('port'), function() {
@@ -33,10 +35,23 @@ gulp.task('serve', function() {
 });
 
 gulp.task('deps', function() {
+
+  // CSS
   gulp.src([
-    'node_modules/highlight.js/styles/github.css'
+    'node_modules/highlight.js/styles/github.css',
+    'node_modules/font-awesome/css/font-awesome.css'
   ]).pipe(concat('dependencies.css'))
+    .pipe(minCSS())
     .pipe(gulp.dest('public/stylesheets'));
+
+  // Fonts
+  gulp.src([
+    'node_modules/font-awesome/fonts/fontawesome-webfont.eot',
+    'node_modules/font-awesome/fonts/fontawesome-webfont.svg',
+    'node_modules/font-awesome/fonts/fontawesome-webfont.ttf',
+    'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
+    'node_modules/font-awesome/fonts/FontAwesome.otf'
+  ]).pipe(gulp.dest('public/fonts'));
 
 });
 
@@ -47,13 +62,13 @@ gulp.task('watch', function() {
 
   lr.listen();
 
-  // Inject style changes for quick iteration
-  gulp.watch('stylesheets/**/*.styl').on('change', compileStyles);
+  gulp.watch('views/**/*.jade', server.changed);
+  gulp.watch('stylesheets/**/*.styl', ['styl']);
 
   // livereload needs to see that it was in fact a CSS file that
   // changed in order to do injection, so I need to watch the css file not the
   // stylus file.
-  gulp.watch('public/stylesheets/style.css').on('change', server.changed);
+  gulp.watch('public/stylesheets/style.css', server.changed);
 });
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('default', ['deps', 'serve', 'watch']);
